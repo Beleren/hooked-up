dfc1c0e687f72bc87129ac637eaf4a76c185c863
Leandro Haruki Futema <leandroharuki@gmail.com>
2020-08-19 21:36:13 -0300

    Adding async useMemo example

--- src/hooks/UseMemo/index.jsx
+++ src/hooks/UseMemo/index.jsx
@@ -1,24 +1,59 @@
-import React, { useMemo } from 'react';
-import { useState } from 'react';
+import React, { useMemo, useState } from 'react';
+import { useEffect } from 'react';
 
-const computeExpensiveValue = (a, b) => {
-  console.log('computing expensive value - should render once');
-  return a + b;
+const SomeLongRunningApi = (arg) => {
+  console.log('running looong api call');
+  return new Promise((res) => {
+    const resolve = () => {
+      const random = Math.random();
+      const mathComparison = random > arg;
+      res(
+        mathComparison
+          ? { interestingProperty: 'so interesting number ' + random }
+          : { interestingProperty: 'very interesting number ' + random }
+      );
+    };
+    setTimeout(resolve, 2000);
+  });
 };
+
 export default function UseMemo() {
   console.log('rendering parent component');
   const [state, setState] = useState(0);
   return (
     <div>
-      <span>parent value: {state}</span>
-      <Hook number={0} />
-      <button onClick={() => setState((state) => state + 1)}>Sum</button>
+      <p>parent value: {state}</p>
+      <button onClick={() => setState((state) => state + 1)}>
+        Sum parent number
+      </button>
+      <Hook number={state} />
     </div>
   );
 }
 
 function Hook({ number }) {
   console.log('rendering child component');
-  const memoizedVal = useMemo(() => computeExpensiveValue(number, 2), [number]);
-  return <div>child value: {memoizedVal}</div>;
+  let [result, setResult] = React.useState('Not yet calculated');
+
+  useEffect(() => {
+    setResult('Not yet calculated');
+  }, [number]);
+
+  const myExpensiveResultObject = useMemo(async () => {
+    const result = await SomeLongRunningApi(number);
+    return result;
+  }, [number]);
+
+  const getResult = async () => {
+    setResult('Not yet calculated');
+    const result = await myExpensiveResultObject;
+    setResult(result.interestingProperty);
+  };
+
+  return (
+    <div>
+      <p>child value: {result}</p>
+      <button onClick={getResult}>Calculate child value</button>
+    </div>
+  );
 }
